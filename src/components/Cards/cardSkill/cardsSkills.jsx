c68c8e74ac747cd4c6714391fd8c6515ec28f785
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2023-09-20 17:44:29 +0500

    modified:   src/components/Cards/cardSkill/cardsSkills.jsx

--- src/components/Cards/cardSkill/cardsSkills.jsx
+++ src/components/Cards/cardSkill/cardsSkills.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function cardsSkills(props) {
     return (
-        <div className='p-4 rounded-lg cursor-pointer hover:bg-slate-200 hover:scale-125 ease-in duration-300'>
+        <div className='p-4 rounded-lg cursor-pointer hover:bg-slate-200 hover:scale-125 ease-in-out duration-100'>
             <img src={`/assets/skills_icons/${props.img}`} alt="" />
         </div>
     )
