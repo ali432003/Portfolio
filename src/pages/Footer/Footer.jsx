d0b5284d13d7fd4bdf9e2ea118836c9cb13836dc
Ali Aamir <111776866+ali432003@users.noreply.github.com>
2024-02-12 19:02:18 +0500

    Update Footer.jsx

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -10,16 +10,16 @@ export default function Footer() {
                 <div className='flex justify-center mt-4'>
 
                     <div className='me-4 hover:scale-125 ease-in duration-300 cursor-pointer'>
-                        <a href="https://github.com/ali432003" target='_blank'> <img src="/assets/github-sign.png" className='p-2 bg-sky-500 rounded-full' width={'55px'} alt="" />
+                        <a > <img src="/assets/github-sign.png" className='p-2 bg-sky-500 rounded-full' width={'55px'} alt="" />
                         </a>
                     </div>
 
                     <div className='mx-4 hover:scale-125 ease-in duration-300 cursor-pointer'>
-                        <a href="https://twitter.com/AliAami24230452" target='_blank'><img src="/assets/twitter.png" className='p-2 bg-sky-500 rounded-full' width={'55px'} alt="" />
+                        <a ><img src="/assets/twitter.png" className='p-2 bg-sky-500 rounded-full' width={'55px'} alt="" />
                         </a>
                     </div>
                     <div className='mx-4 hover:scale-125 ease-in duration-300 cursor-pointer'>
-                       <a href="https://www.facebook.com/profile.php?id=100010615687404" target='_blank'> <img src="/assets/fb.png" className='p-2 bg-sky-500 rounded-full' width={'55px'} alt="" />
+                       <a > <img src="/assets/fb.png" className='p-2 bg-sky-500 rounded-full' width={'55px'} alt="" />
                        </a>
                     </div>
                 </div>
